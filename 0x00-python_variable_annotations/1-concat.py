45d659cac089838d643cba6e965d6131ee085280
Mark Allen Gbalazeh <33788083+Markcino@users.noreply.github.com>
2024-06-10 09:10:56 +0000

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+"""
+Basic annotations - add
+"""
 
-'''Write a type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string'''
 
 def concat(str1: str, str2: str) -> str:
-    """Takes two strings and returns their concatenation"""
+    """
+    function to concat two strings
+    """
     return str1 + str2
