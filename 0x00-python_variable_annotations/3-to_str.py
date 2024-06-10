88b877dd1ab04cae2a4b8686e50597f22d536c7f
Mark Allen Gbalazeh <33788083+Markcino@users.noreply.github.com>
2024-06-10 09:13:14 +0000

    Update 3-to_str.py

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""
+Basic annotations - to string
+"""
 
-'''Write a type-annotated function to_str that takes a float n as
-argument and returns the string representation of the float.
-'''
 
 def to_str(n: float) -> str:
-    """Takes a float and returns the string representation of the float"""
+    """
+    cast float to string
+    """
     return str(n)
